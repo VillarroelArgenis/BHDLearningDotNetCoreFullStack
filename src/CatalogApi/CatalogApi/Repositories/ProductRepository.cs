4c0dabdfa210c8018e9f379159222d2fdd1b0c35
AV119040 <AV119040@LAP0301TRD021>
2020-10-09 17:55:21 -0400

    CatalogAPI

---
+++ src/CatalogApi/CatalogApi/Repositories/ProductRepository.cs
@@ -0,0 +1,85 @@
+﻿using CatalogApi.Data.Interfaces;
+using CatalogApi.Entities;
+using CatalogApi.Repositories.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Repositories
+{
+    public class ProductRepository
+    : IProductRepository
+    {
+        private readonly ICatalogContext context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await context
+                .Products
+                .Find(p => true)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await context
+                .Products
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync();
+
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+            return await context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task Create(Product product)
+        {
+            await context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> Update(Product product)
+        {
+            var updateResult = await context
+                .Products
+                .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged
+                && updateResult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await context
+                .Products
+                .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged
+                && deleteResult.DeletedCount > 0;
+        }
+    }
+}
+
---
+++ src/CatalogApi/CatalogApi/Settings/CatalogDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using CatalogApi.Settings.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Settings
+{
+    public class CatalogDatabaseSettings
+        : ICatalogDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
