4c8af04fb42e4856b758e66a9d164dcf6b10837b
AV119040 <AV119040@LAP0301TRD021>
2020-10-14 15:55:55 -0400

    Init

---
+++ src/BasketApi/BasketApi/Data/BasketContext.cs
@@ -0,0 +1,20 @@
+﻿using BasketApi.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketApi.Data
+{
+    public class BasketContext
+        : IBasketContext
+    {
+        //Redis connect driver
+        public object Redis { get; }
+
+        public BasketContext()
+        {
+            //configura connection
+        }
+    }
+}
---
+++ src/BasketApi/BasketApi/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using BasketApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketApi.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasket(string userName);
+        Task<BasketCart> UpdateBaket(BasketCart basket);
+        Task<bool> DeleteBasket(string userName);
+        Task<bool> Checkout(BasketCheckout basketCheckout);
+    }
+}
